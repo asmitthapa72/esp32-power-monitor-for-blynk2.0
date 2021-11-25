2fbec0368e5156bf0404f6dc5f719a2fc2a5fe07
asmitthapa72 <95006973+asmitthapa72@users.noreply.github.com>
2021-11-24 19:13:45 -0800

    Add files via upload

---
+++ code/Edgent_ESP32/Edgent_ESP32.ino
@@ -0,0 +1,63 @@
+#define BLYNK_TEMPLATE_ID "TMPL0ToNrvn7"
+#define BLYNK_DEVICE_NAME "meterC"
+
+#define BLYNK_FIRMWARE_VERSION        "0.1.0"
+
+#define BLYNK_PRINT Serial
+//#define BLYNK_DEBUG
+
+#define APP_DEBUG
+#include "EmonLib.h"
+
+#include "BlynkEdgent.h"
+
+EnergyMonitor emon;
+#define vCalibration 85
+#define currCalibration 35
+
+BlynkTimer timer;
+
+float kWh = 0;
+unsigned long lastmillis = millis();
+
+void myTimerEvent() {
+     emon.calcVI(20, 2000);
+    Serial.begin(3000);
+    Serial.print("Vrms: ");
+    Serial.print(emon.Vrms, 2);
+    Serial.print("V");
+    Blynk.virtualWrite(V0, emon.Vrms);
+    Serial.print("\tIrms: ");
+    Serial.print(emon.Irms, 4);
+    Serial.print("A");
+    Blynk.virtualWrite(V1, emon.Irms);
+    Serial.print("\tPower: ");
+    Serial.print(emon.apparentPower, 4);
+    Serial.print("W");
+    Blynk.virtualWrite(V2, emon.apparentPower);
+    Serial.print("\tkWh: ");
+    kWh = kWh + emon.apparentPower*(millis()-lastmillis)/3600000000.0;
+    Serial.print(kWh, 4);
+    Serial.println("kWh");
+    lastmillis = millis();
+    Blynk.virtualWrite(V3, kWh);
+
+}
+
+void setup()
+{
+  Serial.begin(3000);
+
+  BlynkEdgent.begin();
+  emon.voltage(35, vCalibration, 1.7); // Voltage: input pin, calibration, phase_shift
+  emon.current(34, currCalibration); // Current: input pin, calibration.
+  timer.setInterval(5L, myTimerEvent);
+ 
+
+}
+
+void loop() {
+  BlynkEdgent.run();
+  timer.run();
+
+}
